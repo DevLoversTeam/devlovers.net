93db2e6d476546f32ab1875be2f2409b1a542185
liudmylasovetovs <milkaegik@gmail.com>
2025-12-17 07:26:15 -0800

    fix: stripe apiVersion

--- frontend/lib/psp/stripe.ts
+++ frontend/lib/psp/stripe.ts
@@ -7,7 +7,7 @@ const { secretKey, webhookSecret, paymentsEnabled, mode } = getStripeEnv()
 
 const stripe = secretKey
   ? new Stripe(secretKey, {
-      apiVersion: "2025-11-17.clover",
+      apiVersion: "2025-12-15.clover",
     })
   : null
 
